c9d1ddca73f81bdf0238e3a0ef94a067ab73b464
Ammar Alifian Fahdan <kerupuksambel.2000@gmail.com>
2021-11-24 01:29:51 +0700

    feat: Geolocation + restoran

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,30 +9,56 @@
 @endsection
 
 @section('content')
-	<div id="map" style="width: 600px; height: 400px;"></div>
+	<div id="map" style="width: 100%; height: 100%;"></div>
 @endsection
 
 @section('js')
 	<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
 	<script src="js/osmtogeojson.js"></script>
 	<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-	<script>
-		var map = L.map('map').setView([-6.1829957,106.8444433], 13);
-		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			// maxZoom: 18,
-			attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a>'
-		}).addTo(map);
-			
+	<script>		
+		var userLat, userLon
+
 		$( document ).ready(function() {
-			var overpassApiUrl = 'https://overpass-api.de/api/interpreter?data=[out:xml][bbox][timeout:180];node[amenity=hospital];out;&bbox=106.7828,-6.2237,106.8695,-6.1469';
-			$.get(overpassApiUrl, function (osmXml) {
-				var OSMGeojson = osmtogeojson(osmXml); 
-				var resultLayer = L.geoJson(OSMGeojson).addTo(map).on('click', showDetail);
-			});
+			if(navigator.geolocation){
+				navigator.geolocation.getCurrentPosition(fromUserLocation, fromDefaultLocation);
+			}
 		});
 
 		function showDetail(e){
-			console.log(e)
+			console.log(e.layer.feature)
+		}
+
+		function fromUserLocation(location){
+			userLon = location.coords.longitude - 0.1;
+			userLat = location.coords.latitude - 0.05;
+
+			getData(userLon, userLat);
+		}
+
+		function fromDefaultLocation(location){
+			userLon = 106.7828;
+			userLat = -6.2237;
+
+			getData(userLon, userLat);
+		}
+
+		function getData(lon, lat){
+			var map = L.map('map').setView([lat + 0.06,lon + 0.06], 13);
+			L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				// maxZoom: 18,
+				attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a>'
+			}).addTo(map);
+			
+			var userBox = lon + "," + lat + "," + (lon + 0.2) + "," + (lat + 0.1)
+
+			var overpassApiUrl = 'https://overpass-api.de/api/interpreter?data=[out:xml][bbox][timeout:180];node[amenity=cafe];out;&bbox=' + userBox;
+			console.log(overpassApiUrl)
+			$.get(overpassApiUrl, function (osmXml) {
+				var OSMGeojson = osmtogeojson(osmXml);
+				console.log(OSMGeojson) 
+				var resultLayer = L.geoJson(OSMGeojson).addTo(map).on('click', showDetail);
+			});
 		}
 	</script>
 @endsection
